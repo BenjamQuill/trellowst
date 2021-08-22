1a0afb56e4cafdac6a1977f7b8959581dd288938
BenjamQuill <benjamin.dujarrier@gmail.com>
2021-08-22 13:40:21 +0000

    cardscontroller ok + tagscontroller ok

--- app/router.js
+++ app/router.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 const ListsController = require('../app/controllers/listsController');
+const CardsController = require('../app/controllers/cardsControlleurs');
+const TagsController = require('../app/controllers/tagsController');
 
 /**
  * Routing for lists
@@ -15,8 +17,26 @@ router.delete('/lists/:id', ListsController.deleteList);
 /**
  * Routing for cards
  */
-router.get('/cards', );
-router.get('/cards/:id', );
+
+router.get('/lists/:id/cards', CardsController.getCardsInList);
+router.get('/cards/:id', CardsController.getOneCard);
+router.post('/cards', CardsController.createCard);
+router.patch('/cards/:id', CardsController.modifyCard);
+router.put('/cards/:id?', CardsController.createOrModify);
+router.delete('/cards/:id', CardsController.deleteCard);
+
+/* Routing for tags */
+router.get('/tags', TagsController.getAllTags);
+router.post('/tags', TagsController.createTag);
+router.patch('/tags/:id', TagsController.modifyTag);
+router.put('/tags/:id?', TagsController.createOrModify);
+router.delete('/tags/:id', TagsController.deleteTag);
+router.post('/cards/:id/tags', TagsController.associateTagToCard);
+router.delete('/cards/:cardId/tags/:tagId', TagsController.removeTagFromCard);
+
+router.use((req, res) => {
+  res.status(404).send('Service does not exists\nSee : https://doc.localhost.api');
+});
 
 module.exports = router;
     
